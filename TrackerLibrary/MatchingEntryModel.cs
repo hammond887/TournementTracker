bbc5c52b44f6a826eacfdc14d1036cda52c35fac
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-07-15 22:24:19 +0100

    update models

---
+++ TrackerLibrary/MatchingEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchingEntryModel
+    {
+        /// <summary>
+        /// Rep one team in matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// rep score for team
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// rep matchup for previous win
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerUI/TournementViewerForm.cs
@@ -0,0 +1,15 @@
+namespace TrackerUI
+{
+    public partial class TournementViewerForm : Form
+    {
+        public TournementViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
